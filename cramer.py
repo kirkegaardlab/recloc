ea5d7dfddd355bf2b172cdc368a37b81961704b9
Albert Alonso <albert.alonso@nbi.ku.dk>
2024-08-19 19:12:37 +0200

    some updates; messy

--- cramer.py
+++ cramer.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from functools import partial
+import json
 import pathlib
+
 import jax
 import jax.numpy as jnp
 import jax.scipy.optimize
@@ -9,20 +11,21 @@ import optax
 from tqdm import tqdm
 
 jax.config.update("jax_enable_x64", True)
-# jax.disable_jit()
 
 a = 1.0  # radius of the cell
-c0 = 15.0  # constant offset in concentration
-d_corr = 1e-3  # correlation distance
-m = 200  # number of surface receptors
-GRADIENT = jnp.array([10.0, 0.0, 0.0])  # gradient vector
-num_iters = 10_000 # number of iterations
+c0 = 100.0  # constant offset in concentration
+D_CORR = 0.002  # correlation distance
+M = 100  # number of surface receptors
+GRADIENT = jnp.array([00.0, 00.0, 10.0])  # gradient vector
+MAX_ITER = 100_000  # number of maximum iterations
+harm_degree = [3]  # degree of the spherical harmonic (l >= m)
+harm_order = 1  # spherical harmonic order (m)
 
 
-def fibonacci_sphere(m):
+def fibonacci_sphere(n):
     phi = jnp.pi * (jnp.sqrt(5) - 1)
-    idxs = jnp.arange(m)
-    y = 1 - idxs / (m - 1) * 2
+    idxs = jnp.arange(n)
+    y = 1 - idxs / (n - 1) * 2
     radius = jnp.sqrt(1 - y * y)
     theta = phi * idxs
     x = jnp.cos(theta) * radius
@@ -32,75 +35,52 @@ def fibonacci_sphere(m):
     return theta, phi
 
 
-def expected_measurements(x, g):
-    return c0 + x @ g
-
-
-def cosine_distance(A, B):
-    cos_similarity = (A @ B.T) / (jnp.linalg.norm(A, axis=1) * jnp.linalg.norm(B, axis=1))
+def distance(X):
+    r = jnp.linalg.norm(X, axis=1)
+    cos_similarity = (X @ X.T) / (r[:, None] * r[None, :])
     cos_distance = 1 - cos_similarity
     return cos_distance
 
 
-def covariance_measurements(X, variance, d_corr=d_corr):
-    D = cosine_distance(X, X)
-    C = jnp.sqrt(variance[:, None] * variance[None, :]) * jnp.exp(-D / (2 * d_corr**2))
-    return C
-
-
-def covariance_measurements_derivative(X, variance, d_corr=d_corr):
-    D = cosine_distance(X, X)
-    exp_term = jnp.exp(-D / (2 * d_corr**2))
-
-    sqrt_term = 1 / (2 * jnp.sqrt(variance[:, None] * variance[None, :]))
-
-    C_c0 = (2 * c0 + (X @ GRADIENT)[:, None] + (X @ GRADIENT)[None, :])
-    C_gx = (X[:, 0][:, None] * (c0 + X @ GRADIENT)[None, :] 
-                      + X[:, 0][None, :] * (c0 + X @ GRADIENT)[:, None])
-    C_gy = (X[:, 1][:, None] * (c0 + X @ GRADIENT)[None, :] 
-                      + X[:, 1][None, :] * (c0 + X @ GRADIENT)[:, None])
-    C_gz = (X[:, 2][:, None] * (c0 + X @ GRADIENT)[None, :] 
-                      + X[:, 2][None, :] * (c0 + X @ GRADIENT)[:, None])
-
-    C_deriv = jnp.stack((C_c0, C_gx, C_gy, C_gz), axis=-1)
-
-    return C_deriv * (exp_term * sqrt_term)[..., None]
-
-
-
-def to_cartesian(r, theta, phi):
-    x = r * jnp.sin(theta) * jnp.cos(phi)
-    y = r * jnp.sin(theta) * jnp.sin(phi)
-    z = r * jnp.cos(theta)
-    return jnp.stack([x, y, z], axis=1)
-
-
-def cramer_rao_bound(X, means, C):
+def cramer_rao_bound(X, G):
+    mu = X @ G  # X={x_0, ..., x_M}, x_i=(1, x, y, z), G=(c0, g_x, g_y, g_z)
+    sigma = jnp.sqrt(mu)
+    spatial_corr = jnp.exp(-distance(X) / (2 * D_CORR**2))
+    C = sigma[:, None] * sigma[None, :] * spatial_corr
     Cinv = jnp.linalg.inv(C)
-    I = X.T @ Cinv @ X
-    
-    C_deriv = covariance_measurements_derivative(X[:, -3:], means)
-    A = jnp.einsum('ij,jka->ika', Cinv, C_deriv)
-    B = jnp.einsum('ija,jkb->ikab', A, A)
+    S = sigma[:, None] / sigma[None, :]
 
-    I = I + 0.5 * jnp.trace(B)
+    @partial(jax.vmap, in_axes=(0, None, 0, 0))
+    @partial(jax.vmap, in_axes=(None, 0, 0, 0))
+    def partial_deriv(xi, xj, s, e):
+        return e / 2 * (xi / s + xj * s)
 
-    uncertainty = jnp.linalg.inv(I)
-    return uncertainty
+    C_deriv = partial_deriv(X, X, S, spatial_corr)
+    A = jnp.einsum("ij,jka->ika", Cinv, C_deriv)
+    B = jnp.einsum("ija,jkb->ikab", A, A)
+    I = X.T @ Cinv @ X + 0.5 * jnp.trace(B)
+    cov_lbound = jnp.linalg.inv(I)
+    return cov_lbound
 
 
 def gradient_estimation_error(X, g):
-    X_hat = jnp.pad(X, ((0, 0), (1, 0)), constant_values=1)
-    means = expected_measurements(X, g)
-    C = covariance_measurements(X, means)
-    covariance_estimations = cramer_rao_bound(X_hat, means, C)
-
-    variance_estimations = jnp.diag(covariance_estimations)[-3:]
+    X_hat = jnp.insert(X, 0, 1.0, axis=1) # X=N * (x, y, z) -> X_hat=N * (1, x, y, z)
+    G_hat = jnp.insert(g, 0, c0, axis=0) # G=(gx, gy, gz) -> G_hat=(c0, gx, gy, gz)
+    covariance_estimations = cramer_rao_bound(X_hat, G_hat)
+    variance_estimations = jnp.diag(covariance_estimations)
     return variance_estimations
 
 
+def to_cartesian(a, theta, phi):
+    r = a + jnp.real(jax.scipy.special.sph_harm(harm_order, harm_degree, phi, theta))
+    x = r * jnp.sin(theta) * jnp.cos(phi)
+    y = r * jnp.sin(theta) * jnp.sin(phi)
+    z = r * jnp.cos(theta)
+    return jnp.stack([x, y, z], axis=-1)
+
+
 @jax.jit
-@partial(jax.value_and_grad, argnums=(0,1))
+@partial(jax.value_and_grad, argnums=(0, 1))
 def objective_fn(thetas, phis):
     X = to_cartesian(a, thetas, phis)
     gradient_uncertainty = gradient_estimation_error(X, GRADIENT)
@@ -110,21 +90,25 @@ def objective_fn(thetas, phis):
 
 def main():
 
-    thetas, phis = fibonacci_sphere(m)
+    thetas, phis = fibonacci_sphere(M)
 
-    output_folder = pathlib.Path('outs') / datetime.now().strftime('%Y%m%d_%H%M%S')
-    ckpt_folder = output_folder / 'checkpoints'
+    output_folder = pathlib.Path("outs") / datetime.now().strftime("%Y%m%d_%H%M%S")
+    ckpt_folder = output_folder / "checkpoints"
     ckpt_folder.mkdir(parents=True, exist_ok=True)
+    params = {
+        "a": a,
+        "harm_order": harm_order,
+        "harm_degree": harm_degree,
+        "gradient": GRADIENT.tolist(),
+    }
+    (output_folder / "params.json").write_text(json.dumps(params, indent=4))
 
     optimizer = optax.adam(1e-3)
     opt_state = optimizer.init((thetas, phis))
     rtol, atol, error_prev = 1e-10, 1e-14, jnp.inf
     metrics = []
-    for i in (bar := tqdm(range(num_iters), ncols=81)):
+    for i in (bar := tqdm(range(MAX_ITER), ncols=81)):
         error, grads = objective_fn(thetas, phis)
-
-        # exit()
-
 
         if i == 0:
             tqdm.write(f"Initial error: {error:.5g}")
@@ -139,6 +123,8 @@ def main():
 
         update, opt_state = optimizer.update(grads, opt_state, (thetas, phis))
         (thetas, phis) = optax.apply_updates((thetas, phis), update)  # pyright: ignore
+        thetas = thetas % jnp.pi
+        phis = phis % (2 * jnp.pi)
 
         if jnp.abs(error - error_prev) < rtol * jnp.abs(error) + atol:
             jnp.savez(ckpt_folder / f"ckpt_{i:06d}.npz", thetas=thetas, phis=phis)
--- visual.py
+++ visual.py
@@ -1,39 +1,80 @@
+import argparse
+import json
 import pathlib
+
 import numpy as np
 import pyvista as pv
+import scipy.spatial
+from tqdm import tqdm
 
-a = 1.0 # Sphere radius
-point_size = 0.2 # Size of the receptors relative to the sphere
+parser = argparse.ArgumentParser()
+parser.add_argument("--point_size", type=float, default=0.1)
+parser.add_argument("--animate", '-a', action='store_true')
+args = parser.parse_args()
 
-def to_cartesian(a, t, p):
-    x = a * np.sin(t) * np.cos(p)
-    y = a * np.sin(t) * np.sin(p)
-    z = a * np.cos(t)
-    return np.vstack([x, y, z]).T
-
+point_size = args.point_size
+CELL_COLOR = [136, 212, 171]
+RECEPTOR_COLOR = [255, 155, 133]
 
 folders = list(pathlib.Path('outs').iterdir())
 last_folder = sorted(folders, key=lambda path: path.stat().st_ctime)[-1]
 files = last_folder.rglob("*.npz")
 files = sorted(files, key=lambda x: int(x.stem.split("_")[1]))
 
+# Load the parameters
+with open(last_folder/'params.json', 'r') as f:
+    params = json.load(f)
+    a = params["a"]
+    harm_order = params["harm_order"]
+    harm_degree = params["harm_degree"]
+    gradient = np.array(params["gradient"])
+
+def to_cartesian(r, theta, phi):
+    r = a + np.real(scipy.special.sph_harm(harm_order, harm_degree, phi, theta))
+    x = r * np.sin(theta) * np.cos(phi)
+    y = r * np.sin(theta) * np.sin(phi)
+    z = r * np.cos(theta)
+    return np.stack([x, y, z], axis=-1)
+
+comparison_files = [files[0], files[-1]]
+
+t, p = np.meshgrid(np.linspace(0, np.pi, 1000), np.linspace(0, 2*np.pi, 1000))
+surface_points = to_cartesian(a, t, p).reshape(-1, 3)
+surface_cloud = pv.PolyData(surface_points)
+arrow = pv.Arrow(np.array([1.8*a, 0, 0]), np.array(gradient)/np.linalg.norm(gradient), scale=0.8)
+
 pl = pv.Plotter(shape=(1,2), window_size=[1800, 800])
-for i, file in enumerate([files[0], files[-1]]):
-    data = np.load(file)
-    t, p = data["thetas"], data["phis"]
-
+for i, file in enumerate(comparison_files):
     pl.subplot(0, i)
-    points = np.array(to_cartesian(a, t, p))
-
-    # Add a sphere representing the cell
-    sphere = pv.Sphere(radius=a, center=(0, 0, 0))
-    pl.add_mesh(sphere, color=[136, 212, 171], smooth_shading=True)
-
-    # Add receptor points on the sphere's surface
+    pl.add_mesh(surface_cloud, color=CELL_COLOR)
+    pl.add_mesh(arrow)
+    data = np.load(file)
+    points = np.array(to_cartesian(a, data["thetas"], data["phis"]))
     receptors = pv.PolyData(points)
-    pl.add_mesh(receptors, render_points_as_spheres=True, point_size=point_size * 100, color=[255, 155, 133])
+    pl.add_mesh(receptors, render_points_as_spheres=True, point_size=point_size * 100, color=RECEPTOR_COLOR)
     pl.add_axes()
     pl.view_isometric()
-
 pl.link_views()
 pl.show()
+
+if not args.animate:
+    exit()
+
+pl = pv.Plotter(window_size=[800, 800], off_screen=False)
+pl.add_mesh(surface_cloud, color=CELL_COLOR)
+pl.add_mesh(arrow)
+data = np.load(files[0])
+points = np.array(to_cartesian(a, data["thetas"], data["phis"]))
+receptors = pv.PolyData(points)
+pl.add_mesh(receptors, render_points_as_spheres=True, point_size=point_size * 100, color=RECEPTOR_COLOR)
+pl.add_axes()
+pl.isometric_view()
+pl.open_gif(last_folder / "animation.gif")
+for f in tqdm(files[1:], ncols=81):
+    data = np.load(f)
+    pl.title_text = f"Frame: {f.stem}"
+    new_points = to_cartesian(a, data["thetas"], data["phis"])
+    receptors.points = new_points
+    pl.write_frame()
+pl.close()
+
